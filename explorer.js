7d513d02ba89762289b79cf66dc47fc336b3d3d1
Daniel Florczak <daniel.florczak@bluesoft.com>
2022-07-25 00:19:50 +0200

    adding /size route

--- explorer.js
+++ explorer.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const Redis = require('redis');
 const app = express();
+let dbSize;
 const redisClient = Redis.createClient();
 redisClient.connect();
-const PORT = 6000;
+const PORT = 3000;
 redisClient.sendCommand(["keys","*"]).then(function(result){
     console.log(result.length);
+    dbSize = result.length;
 });
 
+
+app.get('/size', (req,res)=>{
+    res.send(`db size is ${dbSize}`);
+});
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
