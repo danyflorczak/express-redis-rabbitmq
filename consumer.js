3c2cf12641a655c674629a0c76ab8eeb320b08b1
Daniel Florczak <daniel.florczak@bluesoft.com>
2022-07-24 21:32:49 +0200

    using timestamp as key in set

--- consumer.js
+++ consumer.js
@@ -4,27 +4,29 @@ const amqp =require('amqplib');
 
 const app = express();
 const redisClient = Redis.createClient();
+redisClient.connect();
 const DEFAULT_EXPIRATION = 60;
+
 connect();
 async function connect() {
 
     try {
-        const amqpServer = "amqp://localhost:5672"
-        const connection = await amqp.connect(amqpServer)
+        const amqpServer = "amqp://localhost:5672";
+        const connection = await amqp.connect(amqpServer);
         const channel = await connection.createChannel();
         await channel.assertQueue("jobs");
         
         channel.consume("jobs", message => {
             const input = JSON.parse(message.content.toString());
-            redisClient.set('usrInput', DEFAULT_EXPIRATION, JSON.stringify(input));
+            redisClient.setEx((+ new Date()).toString(), DEFAULT_EXPIRATION, JSON.stringify(input));
             console.log(`Recieved job with input ${JSON.stringify(input)}`);
         })
 
-        console.log("Waiting for messages...")
+        console.log("Waiting for messages...");
     
     }
     catch (ex){
-        console.error(ex)
+        console.error(ex);
     }
 
 }
